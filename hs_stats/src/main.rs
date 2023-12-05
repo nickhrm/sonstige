8116fa87a399edb343ab1220ef6c9cd718d4d30a
nickhrm <nickhrm2308@gmail.com>
2023-12-05 22:21:36 +0100

    recognition now working with single digit

--- hs_stats/src/main.rs
+++ hs_stats/src/main.rs
@@ -1,7 +1,8 @@
-use std::collections::HashMap;
-
 use image::DynamicImage;
 use rusty_tesseract::{Args, Image};
+use std::collections::HashMap;
+use std::fs::OpenOptions;
+use std::io::Write;
 
 const MY_URL: &str = "https://web.hochschulsport-hannover.de/campusfit/auslastungsgrafik";
 
@@ -9,38 +10,34 @@ fn main() {
     let my_args = Args {
         lang: String::from("eng"),
         dpi: Some(150),
-        psm: Some(3),
-        oem: Some(3),
-        config_variables: HashMap::from([(
-            "tessedit_char_whitelist".into(),
-            "1234567890".into(),
-        )]),
+        psm: Some(10),
+        oem: Some(1),
+        config_variables: HashMap::from([("tessedit_char_whitelist".into(), "1234567890".into())]),
     };
 
     match fetch_image() {
-        Ok(mut img) => {
+        Ok(img) => {
+            let mut edited_image = img;
 
-            let cropped_image = img.crop(304, 150, 335, 250);
+            edited_image = edited_image.crop(304, 150, 335, 250);
+            edited_image = edited_image.grayscale();
+            edited_image.save("test.png").unwrap();
 
-            let parsed_image = match Image::from_dynamic_image(&cropped_image) {
+            let parsed_image = match Image::from_dynamic_image(&edited_image) {
                 Ok(img) => img,
                 Err(e) => {
                     println!("Fehler beim Parsen des Bildes: {}", e);
                     return;
                 }
             };
-            let output = rusty_tesseract::image_to_string(&parsed_image, &my_args).unwrap();
+            let mut output: String = rusty_tesseract::image_to_string(&parsed_image, &my_args).unwrap();
+            output = output.chars().filter(|c| !c.is_whitespace()).collect();
             println!("Bild erfolgreich abgerufen. Output: {}", output);
+            write_to_csv(&output).unwrap();
         }
         Err(e) => println!("Es ist ein Fehler aufgetreten: {}", e),
     }
 }
-
-
-
-
-
-
 
 fn fetch_image() -> Result<DynamicImage, String> {
     let img = reqwest::blocking::get(MY_URL);
@@ -55,4 +52,20 @@ fn fetch_image() -> Result<DynamicImage, String> {
         },
         Err(e) => Err(format!("Fehler beim Abrufen des Bildes: {}", e)),
     }
+}
+
+fn write_to_csv(data: &str) -> Result<(), std::io::Error> {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open("output.csv")?;
+
+    writeln!(
+        file,
+        "{},{}",
+        chrono::Utc::now().format("%Y-%m-%d %H:%M:%S"),
+        data
+    )?;
+    Ok(())
 }
