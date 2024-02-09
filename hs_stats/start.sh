67b389396e0a27b1967740babfca5244530bec7f
nickhrm <nickhrm2308@gmail.com>
2024-02-09 22:47:59 +0100

    Add cron job and start script

--- hs_stats/start.sh
+++ hs_stats/start.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 # Starten Sie den Cron-Dienst
-service cron start
+cron
 
 # Führen Sie hier weitere Befehle aus, falls erforderlich
-# Zum Beispiel das Starten Ihres Rust-Programms, falls es sofort laufen soll
-# ./target/release/hs_stats
-
-cargo run --release
+# ...
 
 # Verhindern Sie, dass der Container beendet wird
-# Dies kann erforderlich sein, wenn das Startskript keine lang laufenden Prozesse hat
-while true; do sleep 1000; done
+while true; do sleep 100; done
