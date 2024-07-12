2f5a5c14be258cc3bd4e89c413cf07705389c698
nickhrm <nickhrm2308@gmail.com>
2024-07-13 00:04:43 +0200

    refactor: Improve image processing and data extraction logic

--- getNumbers.py
+++ getNumbers.py
@@ -38,50 +38,72 @@ def download_image():
 #takes image as input and crops it so only the number is visible
 def cropImage():
     image = openImage()
-    y=0
-    x=0
-    h=2000
-    w=1000
+    y=120
+    x=220
+    h=290
+    w=600
     crop = image[y:y+h, x:x+w]
     cv2.imwrite(path, crop)
 
 
+def removeAllButGreen():
+    original_img = openImage()
+    original_img[np.where((original_img!=[64,196,141]).all(axis=2))] = [255,255,255]
+    cv2.imwrite(path, original_img)
+
+
+def rescale():
+    image = openImage()
+    image = cv2.resize(image, None, fx=1.2, fy=1.2, interpolation=cv2.INTER_CUBIC)
+    cv2.imwrite(path, image)
+
+def blurImage():
+    image = openImage()
+    image = (cv2.bilateralFilter(image, 5, 75, 75), 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    cv2.imwrite(path, image)
+
+
+def makeImageGrayScale():
+    image = openImage()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imwrite(path, gray)
 
 #gets number from croped image
 def getNumberFromImage():
     image = openImage()
-    myConfig='--psm 11 --oem 2 -c tessedit_char_whitelist=0123456789'
+    myConfig='--psm 13 --oem 1 -c tessedit_char_whitelist=0123456789'
     text = pytesseract.image_to_string(image, config=myConfig)
     text = re.sub("[^0-9]", "", text)
     return text
 
+#add white border to image so the numer is smaller
+def expandImage():
+    image = openImage()
+    top = 300
+    bottom = 400
+    left = 300
+    right = 400
 
-def removeAllButGreen():
-    original_img = openImage()
-    original_img[np.where((original_img!=[64,196,141]).all(axis=2))] = [255,0,255]
-    cv2.imwrite(path, original_img)
-
-    
-def makeImageGrayScale():
-    im_gray = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    (thresh, im_bw) = cv2.threshold(im_gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    im_bw = cv2.threshold(im_gray, thresh, 255, cv2.THRESH_BINARY)[1]
-    cv2.imwrite(path, im_bw)
-
-
+    image = cv2.copyMakeBorder(image, top, bottom, left, right, cv2.BORDER_CONSTANT, value=[255,255,255])
+    cv2.imwrite(path, image)
 
 def addEntryToCsv(num):
+    if num == "":
+        return
     with open('raw_data.csv', mode='a') as file:
         writer = csv.writer(file)
-        writer.writerow([num, Timestamp.now()])
+        writer.writerow([Timestamp.now(),num])
 
 
 
 
 download_image()
 removeAllButGreen()
-makeImageGrayScale()
+
 cropImage()
+makeImageGrayScale()
+expandImage()
+rescale()
 num = getNumberFromImage()
 print(num)
 addEntryToCsv(num)
